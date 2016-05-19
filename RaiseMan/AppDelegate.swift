cc4981c8ff60627e41ca5fc6e29ce3e0975e0073
Randall Mardus <randalljmardus@gmail.com>
2016-05-19 10:36:11 -0400

    Initial Commit

---
+++ RaiseMan/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RaiseMan
+//
+//  Created by Randall Mardus on 5/19/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
