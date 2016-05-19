d2f85ce2766371dda040c8c9b2d2358db51e472d
Randall Mardus <randalljmardus@gmail.com>
2016-05-19 15:53:52 -0400

    initial commit

---
+++ RaiseMan/Employee.swift
@@ -0,0 +1,15 @@
+//
+//  Employee.swift
+//  RaiseMan
+//
+//  Created by Randall Mardus on 5/19/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Foundation
+
+class Employee : NSObject {
+    var name: String? = "New Employee"
+    var raise: Float = 0.05
+}
+
